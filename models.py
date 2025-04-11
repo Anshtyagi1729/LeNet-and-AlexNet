87ff5c7e3a2b93cd97585c17509ac8a7866bd7c9
AnshTyagi <138436729+Anshtyagi1729@users.noreply.github.com>
2025-04-11 09:58:46 +0530

    updating to have bnLeNet

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 class LeNet(nn.Module):
     def __init__(self,num_classes=10):
         super().__init__()
@@ -34,3 +35,26 @@ class AlexNet(nn.Module):
 
     def forward(self, x):
         return self.net(x)
+class BNLeNet(nn.Module):
+    def __init__(self,num_classes=10):
+        super().__init__()
+        self.net=nn.Sequential(
+            nn.Conv2d(3,6,kernel_size=5,padding=2),nn.BatchNorm2d(6),
+            nn.Sigmoid(),nn.AvgPool2d(kernel_size=2,stride=2),
+            nn.Conv2d(6,16,kernel_size=5),nn.BatchNorm2d(16),nn.Sigmoid(),
+            nn.AvgPool2d(kernel_size=2,stride=2)
+            )
+        with torch.no_grad():
+            dummy=torch.zeros(1,3,32,32)
+            out=self.features(dummy)
+            self.flattened_dim=out.view(1,-1).size(1)
+        self.classifier=nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(self.flattened_dim,120),nn.BatchNorm1d(120),
+            nn.Sigmoid(),nn.Linear(120,84),nn.BatchNorm1d(84),
+            nn.sigmoid(),nn.Linear(84,num_classes)
+        )
+    def forward(self,x):
+        x=self.net(x)
+        x=self.classifier(x)
+        return x
